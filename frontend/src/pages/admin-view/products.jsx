48b6d6126f618d078e9a65d7b027199b4880d372
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-09-24 22:31:00 +0530

    Admin view products page side pop up added

--- frontend/src/pages/admin-view/products.jsx
+++ frontend/src/pages/admin-view/products.jsx
@@ -1,6 +1,61 @@
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { Fragment, useState } from "react";
+
+
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+  averageReview: 0,
+};
+
 function AdminProducts() {
-    return <div>admin Products</div>;
+  const [createProductDialog, setCreateProductDialog] = useState(false);
+  const [formData, setFormData] = useState(initialFormData);
+
+  function onSubmit(event) {
+    event.preventDefault()
   }
-  
-  export default AdminProducts;
-  
+
+  return (
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setCreateProductDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={createProductDialog}
+        onOpenChange={() => {
+          setCreateProductDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <div className="py-6">
+            <CommonForm
+            formData={formData}
+            setFormData={setFormData}
+            buttonText='Add'
+            formControls={addProductFormElements}
+            onSubmit={onSubmit}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
+  );
+}
+
+export default AdminProducts;
